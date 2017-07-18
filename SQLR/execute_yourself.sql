d19d41c01f6eceadffd36aafdccb23513597f400
v-robwhi <v-robwhi@microsoft.com>
2017-07-18 11:18:01 -0700

    Remove Rdemo

--- SQLR/execute_yourself.sql
+++ SQLR/execute_yourself.sql
@@ -5,8 +5,6 @@
 -- 2) The stored procedures should be defined. Open the .sql files for steps 1,2,3 and run "Execute". 
 -- 3) You should connect to the database in the SQL Server of the DSVM with:
 -- - Server Name: localhost
--- - username: rdemo (if you did not change it)
--- - password: D@tascience (if you did not change it)
 
 -- The default table names have been specified. You can modify them by changing the value of the parameters in what follows.
 
@@ -32,19 +30,19 @@ exec [dbo].[splitting] @splitting_percent = 70, @input = 'LoS',  @output = 'Trai
 
 -- Train the models
 exec [dbo].[train_model] @modelName = 'RF',
-                         @connectionString ="Driver=SQL Server;Server=localhost;Database=Hospital;UID=rdemo;PWD=D@tascience",
+                         @connectionString ="Driver=SQL Server;Server=localhost;Database=Hospital;Trusted_Connection=Yes",
 						 @dataset_name = 'LoS',
 						 @training_name = 'Train_Id'
 
 exec [dbo].[train_model] @modelName = 'GBT',
-                         @connectionString ="Driver=SQL Server;Server=localhost;Database=Hospital;UID=rdemo;PWD=D@tascience",
+                         @connectionString ="Driver=SQL Server;Server=localhost;Database=Hospital;Trusted_Connection=Yes",
 						 @dataset_name = 'LoS',
 						 @training_name = 'Train_Id'
 
 -- Test and Evaluate the models. 
 exec [dbo].[test_evaluate_models] @modelrf  = 'RF',
 								  @modelbtree  = 'GBT',
-                                  @connectionString ="Driver=SQL Server;Server=localhost;Database=Hospital;UID=rdemo;PWD=D@tascience", 
+                                  @connectionString ="Driver=SQL Server;Server=localhost;Database=Hospital;Trusted_Connection=Yes", 
 								  @metrics_table_name = 'Metrics',
 								  @dataset_name = 'LoS', 
 								  @training_name = 'Train_Id'
--- SQLR/production_proc.sql
+++ SQLR/production_proc.sql
@@ -4,8 +4,7 @@
 -- 2) The stored procedures should be defined. Open the .sql files for steps 1,2,3 and run "Execute". 
 -- 3) You should connect to the database in the SQL Server of the DSVM with:
 -- - Server Name: localhost
--- - username: rdemo (if you did not change it)
--- - password: D@tascience (if you did not change it)
+
 
 
 -- Set the working database to the one where you created the stored procedures.
