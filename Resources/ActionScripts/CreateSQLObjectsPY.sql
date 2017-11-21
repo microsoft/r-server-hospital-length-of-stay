e4c27a9d0b5dbe4a9b678aaeefaa32578ff659c5
v-robwhi <v-robwhi@microsoft.com>
2017-11-21 11:40:08 -0800

    UpdateSQL Objects

--- Resources/ActionScripts/CreateSQLObjectsPY.sql
+++ Resources/ActionScripts/CreateSQLObjectsPY.sql
@@ -1,4 +1,42 @@
-/****** Object:  View [dbo].[LoS0]    Script Date: 11/21/2017 7:19:58 PM ******/
+USE [Hospital_Py]
+GO
+/****** Object:  Table [dbo].[LengthOfStay]    Script Date: 11/21/2017 7:39:22 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[LengthOfStay](
+	[eid] [int] NOT NULL,
+	[vdate] [datetime] NULL,
+	[rcount] [varchar](2) NULL,
+	[gender] [varchar](1) NULL,
+	[dialysisrenalendstage] [varchar](1) NULL,
+	[asthma] [varchar](1) NULL,
+	[irondef] [varchar](1) NULL,
+	[pneum] [varchar](1) NULL,
+	[substancedependence] [varchar](1) NULL,
+	[psychologicaldisordermajor] [varchar](1) NULL,
+	[depress] [varchar](1) NULL,
+	[psychother] [varchar](1) NULL,
+	[fibrosisandother] [varchar](1) NULL,
+	[malnutrition] [varchar](1) NULL,
+	[hemo] [varchar](1) NULL,
+	[hematocrit] [float] NULL,
+	[neutrophils] [float] NULL,
+	[sodium] [float] NULL,
+	[glucose] [float] NULL,
+	[bloodureanitro] [float] NULL,
+	[creatinine] [float] NULL,
+	[bmi] [float] NULL,
+	[pulse] [float] NULL,
+	[respiration] [float] NULL,
+	[secondarydiagnosisnonicd9] [varchar](2) NULL,
+	[discharged] [datetime] NULL,
+	[facid] [varchar](1) NULL,
+	[lengthofstay] [int] NULL
+) ON [PRIMARY]
+GO
+/****** Object:  View [dbo].[LoS0]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -9,7 +47,7 @@ GO
 		SELECT *
 	    FROM LengthOfStay
 GO
-/****** Object:  Table [dbo].[Stats]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[Stats]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -22,7 +60,7 @@ CREATE TABLE [dbo].[Stats](
 	[std] [float] NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  View [dbo].[LoS]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  View [dbo].[LoS]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -48,7 +86,7 @@ GO
 			   secondarydiagnosisnonicd9, discharged, facid, CAST(lengthofstay as float) lengthofstay
 	    FROM LoS0
 GO
-/****** Object:  Table [dbo].[Boosted_Prediction]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[Boosted_Prediction]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -59,7 +97,7 @@ CREATE TABLE [dbo].[Boosted_Prediction](
 	[eid] [int] NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[ColInfo]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[ColInfo]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -68,7 +106,7 @@ CREATE TABLE [dbo].[ColInfo](
 	[info] [varbinary](max) NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Fast_Prediction]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[Fast_Prediction]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -79,7 +117,7 @@ CREATE TABLE [dbo].[Fast_Prediction](
 	[Score] [float] NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Forest_Prediction]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[Forest_Prediction]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -90,14 +128,14 @@ CREATE TABLE [dbo].[Forest_Prediction](
 	[eid] [int] NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[LoS_Predictions]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[LengthOfStay_Prod]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[LoS_Predictions](
+CREATE TABLE [dbo].[LengthOfStay_Prod](
 	[eid] [int] NOT NULL,
-	[vdate] [date] NULL,
+	[vdate] [datetime] NULL,
 	[rcount] [varchar](2) NULL,
 	[gender] [varchar](1) NULL,
 	[dialysisrenalendstage] [varchar](1) NULL,
@@ -120,16 +158,51 @@ CREATE TABLE [dbo].[LoS_Predictions](
 	[bmi] [float] NULL,
 	[pulse] [float] NULL,
 	[respiration] [float] NULL,
-	[number_of_issues] [varchar](2) NULL,
 	[secondarydiagnosisnonicd9] [varchar](2) NULL,
+	[discharged] [datetime] NULL,
+	[facid] [varchar](1) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[LoS_Predictions]    Script Date: 11/21/2017 7:39:24 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[LoS_Predictions](
+	[eid] [nvarchar](max) NULL,
+	[vdate] [date] NULL,
+	[rcount] [nvarchar](max) NULL,
+	[gender] [nvarchar](max) NULL,
+	[dialysisrenalendstage] [nvarchar](max) NULL,
+	[asthma] [nvarchar](max) NULL,
+	[irondef] [nvarchar](max) NULL,
+	[pneum] [nvarchar](max) NULL,
+	[substancedependence] [nvarchar](max) NULL,
+	[psychologicaldisordermajor] [nvarchar](max) NULL,
+	[depress] [nvarchar](max) NULL,
+	[psychother] [nvarchar](max) NULL,
+	[fibrosisandother] [nvarchar](max) NULL,
+	[malnutrition] [nvarchar](max) NULL,
+	[hemo] [nvarchar](max) NULL,
+	[hematocrit] [nvarchar](max) NULL,
+	[neutrophils] [nvarchar](max) NULL,
+	[sodium] [nvarchar](max) NULL,
+	[glucose] [nvarchar](max) NULL,
+	[bloodureanitro] [nvarchar](max) NULL,
+	[creatinine] [nvarchar](max) NULL,
+	[bmi] [nvarchar](max) NULL,
+	[pulse] [nvarchar](max) NULL,
+	[respiration] [nvarchar](max) NULL,
+	[number_of_issues] [varchar](2) NULL,
+	[secondarydiagnosisnonicd9] [nvarchar](max) NULL,
 	[discharged] [date] NULL,
-	[facid] [varchar](1) NULL,
+	[facid] [nvarchar](max) NULL,
 	[lengthofstay] [float] NULL,
 	[discharged_pred] [date] NULL,
 	[lengthofstay_Pred] [int] NULL
-) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Metrics]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[Metrics]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -143,7 +216,7 @@ CREATE TABLE [dbo].[Metrics](
 	[coefficient_of_determination] [float] NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Models]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[Models]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -153,7 +226,7 @@ CREATE TABLE [dbo].[Models](
 	[model] [varbinary](max) NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[NN_Prediction]    Script Date: 11/21/2017 7:19:58 PM ******/
+/****** Object:  Table [dbo].[NN_Prediction]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -164,7 +237,26 @@ CREATE TABLE [dbo].[NN_Prediction](
 	[Score] [float] NULL
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Train_Id]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  Table [dbo].[RTS]    Script Date: 11/21/2017 7:39:24 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RTS](
+	[id] [nvarchar](255) NULL,
+	[value] [varbinary](255) NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[RTS_Prediction]    Script Date: 11/21/2017 7:39:24 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RTS_Prediction](
+	[lengthofstay_Pred] [float] NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Train_Id]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -175,7 +267,7 @@ CREATE TABLE [dbo].[Train_Id](
 GO
 ALTER TABLE [dbo].[Models] ADD  DEFAULT ('default model') FOR [model_name]
 GO
-/****** Object:  StoredProcedure [dbo].[compute_stats]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[compute_stats]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -265,9 +357,51 @@ BEGIN
 		DEALLOCATE @getname2
 
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[dev_lengthofstay]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[copy_modeling_tables]    Script Date: 11/21/2017 7:39:24 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[copy_modeling_tables]  @dev_db varchar(max) = 'Hospital'
+AS
+BEGIN
+	-- Only copy deployment tables if the production and the deployment databases are different. 
+	DECLARE @database_name varchar(max) = db_name() 
+	IF(@database_name <> @dev_db )
+	BEGIN 
+
+		-- Copy the Stats table into the production database. 
+		 DROP TABLE IF EXISTS [dbo].[Stats]
+		 DECLARE @sql1 nvarchar(max);
+			SELECT @sql1 = N'
+			SELECT *
+			INTO [dbo].[Stats]
+			FROM ['+ @dev_db + '].[dbo].[Stats]';
+			EXEC sp_executesql @sql1;
+
+		-- Copy the Models table into the production database. 
+		 DROP TABLE IF EXISTS [dbo].[Models]
+		 DECLARE @sql2 nvarchar(max);
+			SELECT @sql2 = N'
+			SELECT *
+			INTO [dbo].[Models]
+			FROM ['+ @dev_db + '].[dbo].[Models]';
+			EXEC sp_executesql @sql2;
+
+		-- Copy the ColInfo table into the production database. 
+		 DROP TABLE IF EXISTS [dbo].[ColInfo]
+		 DECLARE @sql3 nvarchar(max);
+			SELECT @sql3 = N'
+			SELECT *
+			INTO [dbo].[ColInfo]
+			FROM ['+ @dev_db + '].[dbo].[ColInfo]';
+			EXEC sp_executesql @sql3;
+	END;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[dev_lengthofstay]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -312,7 +446,7 @@ BEGIN
 
 END
 GO
-/****** Object:  StoredProcedure [dbo].[evaluate]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[evaluate]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -399,9 +533,8 @@ if model_name == "NN":
 , @database_name = @database_name
 ;
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[Exec_Inital_PyScoring]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[Exec_Inital_PyScoring]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -437,7 +570,7 @@ GO
 	----Gradient Boosted Prediction
 	EXEC prediction_results
 GO
-/****** Object:  StoredProcedure [dbo].[Execute_Yourself]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[Execute_Yourself]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -499,7 +632,7 @@ exec [dbo].[evaluate] @model_name  = 'NN',
 
 exec [dbo].[prediction_results]; --- uses Fast_Prediction, outputs to LoS_Predictions
 GO
-/****** Object:  StoredProcedure [dbo].[feature_engineering]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[feature_engineering]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -554,9 +687,8 @@ BEGIN
 
 ;
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[fill_NA_explicit]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[fill_NA_explicit]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -668,9 +800,8 @@ BEGIN
 	CLOSE @getname2
 	DEALLOCATE @getname2
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[fill_NA_mode_mean]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[fill_NA_mode_mean]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -780,9 +911,8 @@ BEGIN
 	CLOSE @getname2
 	DEALLOCATE @getname2
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[get_column_info]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[get_column_info]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -843,9 +973,8 @@ OutputDataSet = DataFrame({"payload": dumps(col_info)}, index=[0])
 , @database_name = @database_name 
 ;
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[prediction_results]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[prediction_results]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -868,9 +997,8 @@ BEGIN
      INTO LoS_Predictions
      FROM LoS JOIN Fast_Prediction ON LoS.eid = Fast_Prediction.eid JOIN LoS0 ON LoS.eid = LoS0.eid;
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[prod_lengthofstay]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[prod_lengthofstay]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -899,7 +1027,7 @@ BEGIN
 
 END
 GO
-/****** Object:  StoredProcedure [dbo].[score]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[score]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -1010,9 +1138,8 @@ if model_name == "NN" and len(model) > 0:
 , @output = @output 
 ;
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[splitting]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[splitting]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -1033,9 +1160,8 @@ BEGIN
   EXEC sp_executesql @sql
 ;
 END
-
 GO
-/****** Object:  StoredProcedure [dbo].[train_model]    Script Date: 11/21/2017 7:19:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[train_model]    Script Date: 11/21/2017 7:39:24 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -1154,3 +1280,127 @@ WHERE model_name = 'default model'
 
 ;
 END
+GO
+/****** Object:  StoredProcedure [dbo].[train_model_real_time_scoring]    Script Date: 11/21/2017 7:39:24 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[train_model_real_time_scoring]   @model_name varchar(20)
+AS 
+BEGIN
+	-- Get the database name and the column information. 
+	DECLARE @info varbinary(max) = (select * from [dbo].[ColInfo]);
+	DECLARE @database_name varchar(max) = db_name();
+
+	-- Train the model on the training set.	
+	EXECUTE sp_execute_external_script @language = N'Python',
+									   @script = N' 
+import dill
+from numpy import sqrt
+from pandas import DataFrame
+from revoscalepy import RxInSqlServer, rx_set_compute_context, RxSqlServerData, rx_dforest, rx_btrees, RxOdbcData, rx_serialize_model, rx_write_object, RxLocalSeq
+from microsoftml import rx_fast_trees, rx_neural_network, adadelta_optimizer
+##########################################################################################################################################
+##	Set the compute context to SQL for faster training
+##########################################################################################################################################
+# Define the connection string
+connection_string = "Driver=SQL Server;Server=localhost;Database=" + database_name + ";Trusted_Connection=true;"
+
+# Set the Compute Context to SQL.
+sql = RxInSqlServer(connection_string = connection_string)
+local = RxLocalSeq()
+rx_set_compute_context(sql)
+
+##########################################################################################################################################
+##	Get the column information.
+##########################################################################################################################################
+column_info = dill.loads(info)
+
+##########################################################################################################################################
+##	Point to the training set and use the column_info list to specify the types of the features.
+##########################################################################################################################################
+variables_all = [var for var in column_info]
+variables_to_remove = ["eid", "vdate", "discharged", "facid"]
+training_variables = [x for x in variables_all if x not in variables_to_remove]
+LoS_Train = RxSqlServerData(sql_query = "SELECT eid, {} FROM LoS WHERE eid IN (SELECT eid from Train_Id)".format(", ".join(training_variables)),
+                            connection_string = connection_string,
+                            column_info = column_info)
+
+##########################################################################################################################################
+##	Specify the variables to keep for the training 
+##########################################################################################################################################
+variables_to_remove = ["eid", "vdate", "discharged", "facid", "lengthofstay"]
+training_variables = [x for x in variables_all if x not in variables_to_remove]
+formula = "lengthofstay ~ " + " + ".join(training_variables)
+
+##########################################################################################################################################
+## Training model based on model selection
+##########################################################################################################################################
+# Parameters of all models have been chosen for illustrative purposes, and can be further optimized.
+
+RTS_odbc = RxOdbcData(connection_string, table = "RTS")
+
+if model_name == "RF":
+	# Train the Random Forest.
+	model = rx_dforest(formula=formula,
+						data=LoS_Train,
+						n_tree=40,
+						cp=0.00005,
+						min_split=int(sqrt(70000)),
+						max_num_bins=int(sqrt(70000)),
+						seed=5)
+	rx_set_compute_context(local)
+	serialized_model = rx_serialize_model(model, realtime_scoring_only = True)
+	rx_set_compute_context(sql)
+	rx_write_object(RTS_odbc, key = "RF", value = serialized_model, serialize = False, compress = None, overwrite = True)
+elif model_name == "GBT":
+	# Train the Gradient Boosted Trees (rx_btrees implementation).
+	model = rx_btrees(formula=formula,
+						data=LoS_Train,
+						n_tree=40,
+						learning_rate=0.3,
+						cp=0.00005,
+						loss_function="gaussian",
+						min_split=int(sqrt(70000)),
+						max_num_bins=int(sqrt(70000)),
+						seed=9)
+	rx_set_compute_context(local)
+	serialized_model = rx_serialize_model(model, realtime_scoring_only = True)
+	rx_set_compute_context(sql)
+	rx_write_object(RTS_odbc, key = "GBT", value = serialized_model, serialize = False, compress = None, overwrite = True)
+elif model_name == "FT":
+	# Train the Fast Trees (rx_fast_trees implementation).
+	model = rx_fast_trees(formula=formula,
+                          data=LoS_Train,
+                          num_trees=40,
+                          method="regression",
+                          learning_rate=0.2,
+                          split_fraction=5/24,
+                          min_split=10)
+	rx_set_compute_context(local)
+	serialized_model = rx_serialize_model(model, realtime_scoring_only = True)
+	rx_set_compute_context(sql)
+	rx_write_object(RTS_odbc, key = "FT", value = serialized_model, serialize = False, compress = None, overwrite = True)
+else:
+	# Train the Neural Network (rx_neural_network implementation).
+	model = rx_neural_network(formula=formula,
+                            data=LoS_Train,
+                            method = "regression",
+                            num_hidden_nodes = 128,
+                            num_iterations = 100,
+                            optimizer = adadelta_optimizer(),
+                            mini_batch_size = 20)
+	rx_set_compute_context(local)
+	serialized_model = rx_serialize_model(model, realtime_scoring_only = True)
+	rx_set_compute_context(sql)
+	rx_write_object(RTS_odbc, key = "NN", value = serialized_model, serialize = False, compress = None, overwrite = True)
+'
+, @params = N' @model_name varchar(20), @info varbinary(max), @database_name varchar(max)'
+, @model_name = @model_name
+, @info = @info
+, @database_name = @database_name
+;
+END
+GO
