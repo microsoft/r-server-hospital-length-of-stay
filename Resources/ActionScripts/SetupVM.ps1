1b6b3d2c1a20e646b96f2e73bbf4faeaf88cf4a5
v-robwhi <v-robwhi@microsoft.com>
2017-12-12 14:24:31 -0800

    playing with params

--- Resources/ActionScripts/SetupVM.ps1
+++ Resources/ActionScripts/SetupVM.ps1
@@ -140,7 +140,7 @@ $shortcut.TargetPath = $solutionPath
 $shortcut.Save()
 
 
-Invoke-Expression C:\Solutions\Hospital\Resources\ActionScripts\ConfigureSQL.ps1 -serverName $ServerName , -SolutionName $SolutionName
+Invoke-Expression C:\Solutions\Hospital\Resources\ActionScripts\ConfigureSQL.ps1  $ServerName ,  $SolutionName
 
 
 
