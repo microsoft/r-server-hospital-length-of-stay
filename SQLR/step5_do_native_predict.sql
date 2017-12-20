37e8ca8a9cd217c546a97654ab3c9ef0a17b5d3b
sgilley <sgilley@microsoft.com>
2017-12-20 09:29:10 -0600

    add clean, featurize

--- SQLR/step5_do_native_predict.sql
+++ SQLR/step5_do_native_predict.sql
@@ -4,8 +4,10 @@
 
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 CREATE PROCEDURE [dbo].[do_native_predict]
     @eid int
@@ -23,21 +25,29 @@ BEGIN
 
 
     -- Get the patient record from a historical table using the eid
+	INSERT INTO [QueryPatient]
     SELECT *
-    INTO [#QueryPatient]
-    FROM [LoS]
+    FROM [LengthOfStay]
     WHERE eid = @eid
+
+--	Step 1: Replace the missing values with the mode and the mean. 
+	exec [dbo].[fill_NA_mode_mean] @input = [QueryPatient], @output = 'LoS0_Prod'
+
+-- Step 2: Feature Engineering. 
+    exec [dbo].[feature_engineering]  @input = 'LoS0_Prod', @output = 'Los_Prod', @is_production = 1
+
 
     SET @start = GETDATE()
 
     -- Do real time scoring using native PREDICT clause
     SELECT [LengthOfStay_Pred]
-    FROM PREDICT (MODEL = @nativeModel, DATA = [#QueryPatient] ) WITH ( LengthOfStay_Pred FLOAT ) p;
+    FROM PREDICT (MODEL = @nativeModel, DATA = [LoS_Prod] ) WITH ( LengthOfStay_Pred FLOAT ) p;
 
     SET @end = GETDATE()
 
     SET @elapsed = CONVERT(VARCHAR(max),(SELECT DATEDIFF(MICROSECOND,@start,@end)))
 
     PRINT 'Elapsed Time for 1 row scoring is : ' + @elapsed + ' microseconds.'
-END
+	TRUNCATE table QueryPatient 
 
+END
