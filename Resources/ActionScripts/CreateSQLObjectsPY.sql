4237b113cdb8c7b7464e3d159f585d05872bdd7b
Hiram Fleitas <hfleitas@universalproperty.com>
2021-02-09 12:58:25 -0500

    fixed proc name GetRTSModelRR to GetRTSModelRF

--- Resources/ActionScripts/CreateSQLObjectsPY.sql
+++ Resources/ActionScripts/CreateSQLObjectsPY.sql
@@ -1460,3 +1460,54 @@ CREATE TABLE [dbo].[QueryPatient](
 	[lengthofstay] [int] NULL
 ) ON [PRIMARY]
 GO
+
+-- My simple proc to serialize the model bin, cause train_model_real_time_scoring errors. 
+
+create or alter proc [GetRTSModelRF]   
+as
+
+declare @info varbinary(max);
+select @info = info from dbo.ColInfo;
+
+exec sp_execute_external_script @language = N'Python', @script = N' 
+import dill
+from numpy import sqrt
+from pandas import DataFrame
+from revoscalepy import rx_set_compute_context, RxSqlServerData, rx_dforest, RxOdbcData, rx_serialize_model, rx_write_object, RxLocalSeq
+from microsoftml import adadelta_optimizer
+
+connection_string = "Driver=SQL Server;Server=localhost;Database=Hospital_Py;Trusted_Connection=true;"
+
+column_info = dill.loads(info)
+
+##	Set training dataset, set features and types.
+
+variables_all = [var for var in column_info]
+variables_to_remove = ["eid", "vdate", "discharged", "facid"]
+training_variables = [x for x in variables_all if x not in variables_to_remove]
+LoS_Train = RxSqlServerData(sql_query = "SELECT eid, {} FROM LoS WHERE eid IN (SELECT eid from Train_Id)".format(", ".join(training_variables)),
+                            connection_string = connection_string,
+                            column_info = column_info)
+
+##	Specify the variables to keep for the training 
+
+variables_to_remove = ["eid", "vdate", "discharged", "facid", "lengthofstay"]
+training_variables = [x for x in variables_all if x not in variables_to_remove]
+formula = "lengthofstay ~ " + " + ".join(training_variables)
+
+## Train RF Model
+dest = RxOdbcData(connection_string, table = "RTS")
+model = rx_dforest(formula=formula,
+                    data=LoS_Train,
+                    n_tree=40,
+                    cp=0.00005,
+                    min_split=int(sqrt(70000)),
+                    max_num_bins=int(sqrt(70000)),
+                    seed=5)
+serialized_model = rx_serialize_model(model, realtime_scoring_only = True)
+rx_write_object(dest, key_name="id", key="RF", value_name="value", value=serialized_model, serialize=False, compress=None, overwrite=False)'
+
+, @params = N'@info varbinary(max)'
+, @info = @info;
+
+GO
