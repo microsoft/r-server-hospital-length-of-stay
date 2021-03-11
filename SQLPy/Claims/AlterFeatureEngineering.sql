87139ae81630a1315a86975793947394f6811deb
Hiram Fleitas <hfleitas@universalproperty.com>
2021-03-11 18:53:37 -0500

    error with python formula in Train model.

--- SQLPy/Claims/AlterFeatureEngineering.sql
+++ SQLPy/Claims/AlterFeatureEngineering.sql
@@ -35,12 +35,13 @@ BEGIN
                 StatesStateCode, 
                 LossTypeDescription, 
                 PolicyVersionAttributesFormType, 
-                PolicyVersionAttributesOccupancyType, 			   
+                PolicyVersionAttributesOccupancyType,
+				ClaimMoneyReserve, 			   
 		       (PolicyVersionAttributesCoverageA - (SELECT mean FROM [dbo].[Stats] WHERE variable_name = ''PolicyVersionAttributesCoverageA''))/(SELECT std FROM [dbo].[Stats] WHERE variable_name = ''PolicyVersionAttributesCoverageA'') AS PolicyVersionAttributesCoverageA,
-		       --(ClaimMoneyReserve - (SELECT mean FROM [dbo].[Stats] WHERE variable_name = ''ClaimMoneyReserve''))/(SELECT std FROM [dbo].[Stats] WHERE variable_name = ''ClaimMoneyReserve'') AS ClaimMoneyReserve,
 		       (ClaimMoneyLosses - (SELECT mean FROM [dbo].[Stats] WHERE variable_name = ''ClaimMoneyLosses''))/(SELECT std FROM [dbo].[Stats] WHERE variable_name = ''ClaimMoneyLosses'') AS ClaimMoneyLosses,
 		       (ClaimMoneyLAE - (SELECT mean FROM [dbo].[Stats] WHERE variable_name = ''ClaimMoneyLAE''))/(SELECT std FROM [dbo].[Stats] WHERE variable_name = ''ClaimMoneyLAE'') AS ClaimMoneyLAE,
-               ClaimRoomsWithDamage AS number_of_issues,
+   		       (ClaimRoomsWithDamage - (SELECT mean FROM [dbo].[Stats] WHERE variable_name = ''ClaimRoomsWithDamage''))/(SELECT std FROM [dbo].[Stats] WHERE variable_name = ''ClaimRoomsWithDamage'') AS ClaimRoomsWithDamage,
+			   ClaimRoomsWithDamage AS number_of_issues,
 			   ClaimDateClosed,'+
 			   (CASE WHEN @is_production = 0 THEN 'CAST(lengthofstay as float) lengthofstay' else 'NULL as lengthofstay' end) + '
 	    FROM ' + @input;
